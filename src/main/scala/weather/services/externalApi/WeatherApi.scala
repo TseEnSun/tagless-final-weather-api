f79aafca769dc18fa38605d041dba63481a8e25d
Sean Sun <tseensun@gmail.com>
2023-03-16 21:58:07 +0800

    Add weather api interpreter

--- src/main/scala/weather/services/externalApi/WeatherApi.scala
+++ src/main/scala/weather/services/externalApi/WeatherApi.scala
@@ -1,8 +1,48 @@
 package weather.services.externalApi
 
-import org.http4s.client.Client
+import cats.implicits._
+import cats.effect._
+import org.http4s._
+import org.http4s.client._
+import org.http4s.dsl.io._
+import org.http4s.implicits._
+import org.http4s.circe._
+import io.circe._
+import io.circe.generic.semiauto._
+import weather.domain.City
+import weather.domain.Weather
+import weather.domain.Weather._
+
 
 object WeatherApi{
-  def make[F[_]](client: Client[F]): WeatherApiAlgebra[F] = ???
+
+  final case class WeatherApiResponse(current: Weather)
+  implicit val weatherApiResponseDecoder: Decoder[WeatherApiResponse] = deriveDecoder
+
+  def make[F[_]: Concurrent](client: Client[F]): WeatherApiAlgebra[F] = 
+
+    implicit val weatherApiResponseEntityDecoder: EntityDecoder[F, WeatherApiResponse] = jsonOf[F, WeatherApiResponse]
+    new WeatherApiAlgebra[F] {
+      val weatherApiToken = "25393e6d0e674ae0a1e54447231203"
+      val baseUri = uri"https://api.weatherapi.com"
+      val uriWithPath = baseUri.withPath(path"/v1/current.json")
+      def get(city: City): F[Either[String, Weather]] = {
+        val uriWithPathAndQuery = 
+          uriWithPath.withQueryParams(Map("q" -> city.name, "key" -> weatherApiToken))
+        val request = Request[F](
+          method = GET,
+          uri = uriWithPathAndQuery
+        )
+
+        client.run(request).use {
+          case Status.Successful(resp) => 
+            resp.asJsonDecode[WeatherApiResponse].map(x => Right(x.current))
+          case resp => 
+            resp.as[String].map(msg => Left(msg))
+        }
+        
+      }
+    }
+
 }
   
--- src/main/scala/weather/services/externalApi/WeatherApiAlgebra.scala
+++ src/main/scala/weather/services/externalApi/WeatherApiAlgebra.scala
@@ -1,8 +1,9 @@
 package weather.services.externalApi
 
 import weather.domain.Weather
+import weather.domain.City
 
 trait WeatherApiAlgebra[F[_]] {
-  def get(params: Map[String, String]): F[Weather]
+  def get(city: City): F[Either[String, Weather]]
 }
   
