ba66af7574006773c8aa07b2246f7f5bf0652d39
Sean Sun <tseensun@gmail.com>
2023-03-22 16:05:51 +0800

    Add embedding example from papers

--- src/main/scala/embedding/TypeClassFinal.scala
+++ src/main/scala/embedding/TypeClassFinal.scala
@@ -7,22 +7,28 @@ object TypeClassFinal {
     def neg(e: repr): repr
     def add(e1: repr , e2: repr): repr
 
-  object ExpSYMInt extends ExpSYM[Int]:
+  given ExpSYM[Int] with
     def lit(n: Int) = n
     def neg(e: Int) = -e
     def add(e1: Int, e2: Int) = e1 + e2
 
-  object ExpSYMString extends ExpSYM[String]:
+  given ExpSYM[String] with
     def lit(n: Int) = s"$n"
     def neg(e: String) = s"(-$e)"
     def add(e1: String, e2: String) = s"($e1+$e2)" 
 
-  def tf1[A](exp: ExpSYM[A]): A = exp.add(exp.lit(8), exp.neg(exp.add(exp.lit(1), exp.lit(2))))
+  def tf1[A](using exp: ExpSYM[A]): A = exp.add(exp.lit(8), exp.neg(exp.add(exp.lit(1), exp.lit(2))))
+
+  def eval(in: Int): Int = identity(in)
+  def view(str: String): String = identity(str)
 
   def main(args: Array[String]): Unit = {
     println("-" * 15)
-    println(s"tf1_eval: ${tf1(ExpSYMInt)}")
-    println(s"tf1_view: ${tf1(ExpSYMString)}")
+    println(s"tf1_eval: ${eval(tf1)}")
+    println(s"tf1_view: ${view(tf1)}")
+    println("-" * 15)
+    println(s"tf1_as_Int: ${tf1[Int]}")
+    println(s"tf1_as_String: ${tf1[String]}")
     println("-" * 15)
   }
 
