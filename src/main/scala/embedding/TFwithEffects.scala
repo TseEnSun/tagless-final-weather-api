5937aec0a202e42d3a886c0f0499313d3de5c091
Sean Sun <tseensun@gmail.com>
2023-09-29 22:03:10 +0800

    update example code

---
+++ src/main/scala/embedding/TFwithEffects.scala
@@ -0,0 +1,8 @@
+package embedding
+
+import TypeClassFinal.*
+
+object TFwithEffects {
+  // Create an exampple to show how to use the effects
+  
+}
--- src/main/scala/embedding/TypeClassFinalExtension.scala
+++ src/main/scala/embedding/TypeClassFinalExtension.scala
@@ -15,8 +15,9 @@ object TypeClassFinalExtension {
   given MulSYM[String] with
     def mul(e1: String, e2: String) = s"($e1*$e2)"
   
-  // def tf2[A](using exp: ExpSYM[A], mul: MulSYN[A]): A = mul.mul(exp.lit(8), exp.neg(mul.mul(exp.lit(1), exp.lit(2))))
-  def tf2[A: ExpSYM : MulSYM] = MulSYM[A].mul(ExpSYM[A].lit(8), ExpSYM[A].neg(MulSYM[A].mul(ExpSYM[A].lit(1), ExpSYM[A].lit(2))))
+  def tf2[A](using exp: ExpSYM[A], mul: MulSYM[A]): A = mul.mul(exp.lit(8), exp.neg(mul.mul(exp.lit(1), exp.lit(2))))
+
+  def tf2Alter[A: ExpSYM : MulSYM] = MulSYM[A].mul(ExpSYM[A].lit(8), ExpSYM[A].neg(MulSYM[A].mul(ExpSYM[A].lit(1), ExpSYM[A].lit(2))))
 
   def main(args: Array[String]): Unit = {
     println("-" * 15)
